34d1335f6c8aac13b23853700a4ed046d4d1ea0b
ichi <w.konrad60@gmail.com>
2017-11-01 13:13:19 +0100

    zadanie 13.7

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var OSinfo = require('./OSinfo');
-
+var fs = require('fs');
+var colors = require('colors');
 
 process.stdin.setEncoding('utf-8');
 
+/*fs.readFile('./text.txt', 'utf8', function(err, data) {
+	console.log('Dane przed zapisem!'.blue);
+	console.log(data);
+	fs.appendFile('./text.txt', '\njajko', function(err) {
+		if (err) throw err;
+		console.log('Zapisano');
+		fs.readFile('./text.txt', 'utf-8', function(err, data) {
+			console.log('Dane po zapisie'.blue);
+			console.log(data);
+		});
+	});
+});*/
 
+
+fs.readdir('./', 'utf-8', function(err, files) {
+	fs.writeFile('./text.txt', files, function(err) {
+		if (err) throw err;
+		console.log('Zapisano'.green);
+	});
+});
 
 console.log(process.env.username);
 //wyświetlony użytkownik, ponieważ w systemie windows nie wyświetlimy języka sytemu :)
