d32fbdddbb5270eb62351420da1a92c4820870f9
ichi <w.konrad60@gmail.com>
2017-11-03 21:18:39 +0100

    zadanie13.8

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-var OSinfo = require('./OSinfo');
 var fs = require('fs');
-var colors = require('colors');
 
-process.stdin.setEncoding('utf-8');
+var http = require('http');
+var server = http.createServer();
+var indexHTML = "";
+var image;
 
-/*fs.readFile('./text.txt', 'utf8', function(err, data) {
-	console.log('Dane przed zapisem!'.blue);
-	console.log(data);
-	fs.appendFile('./text.txt', '\njajko', function(err) {
-		if (err) throw err;
-		console.log('Zapisano');
-		fs.readFile('./text.txt', 'utf-8', function(err, data) {
-			console.log('Dane po zapisie'.blue);
-			console.log(data);
-		});
+
+fs.readFile('./index.html', 'utf-8', function(err, data) {
+	indexHTML = data;
+});
+fs.readFile('./404.jpg', function(err, data) {
+	image = data;
+})
+
+server.on('request', function(request, response) {
+	if (request.method === 'GET' && request.url === '/s'){
+		response.setHeader("Content-Type", "text/html; charset=utf-8");
+		response.end(indexHTML);
+	} else {
+		response.setHeader("Content-Type", "image/jpeg; charset=utf-8");
+		response.end(image);
+	}
+});
+
+/*fs.readFile('./index.html', 'utf-8', function(err, http) {
+	if(err) throw err;
+		server.on('request', function(request, response) {
+		response.setHeader("Content-Type", "text/html; charset=utf-8");
+			if (request.method === 'GET' && request.url === '/s') {
+				response.write(http);
+				response.end();
+			} else {
+				fs.readFile('./404.html', 'utf-8', function(err, http) {
+					response.statusCode = 404;
+					response.write(http);
+					response.end();
+				});
+		}
 	});
 });*/
 
 
-fs.readdir('./', 'utf-8', function(err, files) {
-	fs.writeFile('./text.txt', files, function(err) {
-		if (err) throw err;
-		console.log('Zapisano'.green);
-	});
-});
+server.listen(8080);
 
-console.log(process.env.username);
-//wyświetlony użytkownik, ponieważ w systemie windows nie wyświetlimy języka sytemu :)
-
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-
-    if (input !== null) {
-        var instruction = input.toString().trim();
-
-        switch(instruction) {
-            case '':
-                process.stderr.write('Instruction is empty. Please repeat');
-                break;
-            case  '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/getOSinfo':  
-                OSinfo.print();
-                break;
-            default: 
-                process.stderr.write('Wrong instruction!\n');
-                break;
-        }
-        //process.stdout.write(input);
-    }
-   
-});
 
