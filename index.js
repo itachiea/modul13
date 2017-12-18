3100c35a673cc13749c44715c10e420853fd1da2
ichi <w.konrad60@gmail.com>
2017-12-18 18:47:00 +0100

    poprawki

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+ var imageToTextDecoder = require('image-to-text');
 var http = require('http');
 var server = http.createServer();
 var indexHTML = "";
@@ -12,7 +12,7 @@ fs.readFile('./index.html', 'utf-8', function(err, data) {
 fs.readFile('./404.jpg', function(err, data) {
 	image = data;
 })
-
+console.log(image);
 server.on('request', function(request, response) {
 	if (request.method === 'GET' && request.url === '/'){
 		response.setHeader("Content-Type", "text/html; charset=utf-8");
@@ -25,5 +25,3 @@ server.on('request', function(request, response) {
 
 
 server.listen(8080);
-
-
